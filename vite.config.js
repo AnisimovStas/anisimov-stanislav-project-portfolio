c7d19ed5996010a7a67a8c61200614fecb828906
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-02-20 22:58:12 +0300

    config setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
+const path =require('path')
 export default defineConfig({
+  alias:{
+    '@': path.resolve(__dirname, '/src')
+  },
   plugins: [vue()],
 })
