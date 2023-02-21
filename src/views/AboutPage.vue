522b0650bc0e185400e773fe21ac8e47b04b3de8
Feralbot <staaasyyaaan@gmail.com>
2023-02-21 14:03:24 +0300

    added page about

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -2,23 +2,44 @@
 <template>
   <div class="about">
     <div class="about-content">
-      <h1>ABOUT</h1>
-      <p>
-        Hello, my name is Stanislav Anisimov, im frontend web developer and web
-        designer.
-      </p>
-      <p>
-        After graduating with MA and 3 years in building estimates works
-        expirience i'm become self-taught front-end web development.
-      </p>
-      <p>
-        Currently has over 1 years of experience in mainly CSS, HTML
-        (TailwindCSS, SCSS) and JavaScript(Vue.js ecosystem).
-      </p>
-      <p>
-        Obsessed with application performance, user experience, and simplicity.
-      </p>
-      <p>At this web portfolio you may see showcases of my works. Enjoy!</p>
+      <div class="about-content-photo">
+        <div class="photo">
+          <img
+            class="photo"
+            src="@/assets/myPhoto.jpeg"
+            alt="photo placeholder"
+          />
+        </div>
+        <div class="photo__description">
+          Anisimov Stanislav
+          <p>Frontend developer</p>
+          <p>26 y/o</p>
+        </div>
+      </div>
+      <div class="about-content-text">
+        <h1 class="about__title">ABOUT</h1>
+        <br />
+        <p class="about__text">
+          Hello, my name is Stanislav Anisimov, im frontend web developer and
+          web designer.
+        </p>
+        <p class="about__text">
+          After graduating with MA and 3 years in building estimates works
+          expirience i'm become self-taught front-end web development.
+        </p>
+        <p class="about__text">
+          Currently has over 1 years of experience in mainly CSS, HTML
+          (TailwindCSS, SCSS) and JavaScript(Vue.js ecosystem).
+        </p>
+        <p class="about__text">
+          Obsessed with application performance, user experience, and
+          simplicity.
+        </p>
+        <p class="about__text">
+          At this web portfolio you may see showcases of my works.
+          <span style="font-weight: bold">Enjoy!</span>
+        </p>
+      </div>
     </div>
   </div>
   <div class="contacts">
