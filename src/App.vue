16fa0cbcb8aec6c5dbd68d7bad8eac52c3cb8e5e
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-02-24 19:47:30 +0300

    adding base text lang switch

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
 import { ref } from "vue";
 import projectsData from "./data/projects.json";
-const sidebarShow = ref(false);
+import { useI18n } from "vue-i18n";
+const { t, locale } = useI18n({ useScope: "global" });
 const isActive = ref(false);
 const projects = ref(projectsData);
+const languageSwitch = () => {
+  locale.value === "en" ? (locale.value = "ru") : (locale.value = "en");
+};
 </script>
 
 <template>
@@ -11,15 +15,15 @@ const projects = ref(projectsData);
     <div class="portfolio-header-content">
       <RouterLink to="/">
         <div class="portfolio-header-title">
-          <p class="portfolio-header-title__mainText">Anisimov Stanislav</p>
-          <p class="portfolio-header-title__text">project portfolio</p>
+          <p class="portfolio-header-title__mainText">{{ t("titleSTR1") }}</p>
+          <p class="portfolio-header-title__text">{{ t("titleSTR2") }}</p>
         </div>
       </RouterLink>
       <!-- При фуллскрине -->
       <nav class="portfolio-nav">
         <ul class="portfolio-nav__list">
           <RouterLink to="/about">
-            <li class="portfolio-nav__item">About</li></RouterLink
+            <li class="portfolio-nav__item">{{ t("about") }}</li></RouterLink
           >
           <li v-for="project in projects" :key="project">
             <RouterLink :to="{ path: project.id }">
@@ -37,32 +41,38 @@ const projects = ref(projectsData);
         <div class="portfolio-links__language-switcher language-switcher">
           <div class="language-switcher">
             <button
-              @click="isActive = !isActive"
-              :disabled="!isActive"
-              :class="{ 'language-switcher-button bounce': isActive }"
+              @click="
+                isActive = !isActive;
+                languageSwitch();
+              "
+              :disabled="isActive"
+              :class="{ 'language-switcher-button bounce': !isActive }"
             >
               <img
                 src="./assets/russia_icon.png"
                 :class="{
                   ' language-switcher-button__image language-switcher-button__image--active':
-                    !isActive,
-                  ' language-switcher-button__image language-switcher-button__image--disabled':
                     isActive,
+                  ' language-switcher-button__image language-switcher-button__image--disabled':
+                    !isActive,
                 }"
               />
             </button>
             <button
-              @click="isActive = !isActive"
-              :disabled="isActive"
-              :class="{ 'language-switcher-button bounce': !isActive }"
+              @click="
+                isActive = !isActive;
+                languageSwitch();
+              "
+              :disabled="!isActive"
+              :class="{ 'language-switcher-button bounce': isActive }"
             >
               <img
                 src="https://i.ibb.co/KjQvWN3/usa-icon.png"
                 :class="{
                   'language-switcher-button__image language-switcher-button__image--active':
-                    isActive,
-                  ' language-switcher-button__image language-switcher-button__image--disabled':
                     !isActive,
+                  ' language-switcher-button__image language-switcher-button__image--disabled':
+                    isActive,
                 }"
               />
             </button>
--- src/main.js
+++ src/main.js
@@ -3,10 +3,22 @@ import router from "./router";
 import "./styles/index.scss";
 import App from "./App.vue";
 import { createI18n, useI18n } from "vue-i18n";
+import { defaultLocale, languages } from "./i18n";
 
+const messages = Object.assign(languages);
 const i18n = createI18n({
   legacy: false,
+  locale: defaultLocale,
   fallbackLocale: "en",
+  messages,
 });
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App, {
+  setup() {
+    const { t } = useI18n;
+    return { t };
+  },
+});
+app.use(router);
+app.use(i18n);
+app.mount("#app");
