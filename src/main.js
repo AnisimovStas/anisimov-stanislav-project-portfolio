8055831a8ad5262435ae6690e30aca9e369864ea
Anisimov Stanislav <stanislav.anisimov.dev@gmail.com>
2023-02-24 19:02:22 +0300

    adding i18n

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import { createApp } from "vue";
 import router from "./router";
-
 import "./styles/index.scss";
 import App from "./App.vue";
+import { createI18n, useI18n } from "vue-i18n";
+
+const i18n = createI18n({
+  legacy: false,
+  fallbackLocale: "en",
+});
 
 createApp(App).use(router).mount("#app");
